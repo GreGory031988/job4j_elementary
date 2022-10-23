d9e21f4d3578f22cafe11003fb5b4e1bf30d7076
Григорий <grinrb@mail.ru>
2022-10-23 20:19:39 +0500

    6.0.2. Размер массива [#333582 #352622]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -6,7 +6,7 @@ public class ChessBoard {
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
             if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
-                rsl = rsl == 0 ? Math.abs(x2 - x1) : rsl;
+                rsl = Math.abs(x2 - x1);
             }
         }
         return rsl;
