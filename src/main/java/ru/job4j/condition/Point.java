d0e7b14904fef86d9053516e22daf15ee13cd4b5
Григорий <grinrb@mail.ru>
2022-11-12 23:11:30 +0500

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,18 +2,17 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return d;
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
         public static void main(String[] args) {
-        double d = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) are " + d);
-        double d1 = Point.distance(0, 0, 3, 4);
-        System.out.println("result (0, 0) to (2, 2) are " + d1);
-        double d2 = Point.distance(2, 3, 2, 2);
-        System.out.println("result (2, 3) to (2, 2) are " + d2);
-        double d3 = Point.distance(1, 0, 7, 0);
-        System.out.println("result (1, 0) to (7, 0) are " + d3);
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0, 0) to (2, 0) are " + result);
+        double result1 = Point.distance(0, 0, 3, 4);
+        System.out.println("result (0, 0) to (2, 2) are " + result1);
+        double result2 = Point.distance(2, 3, 2, 2);
+        System.out.println("result (2, 3) to (2, 2) are " + result2);
+        double result3 = Point.distance(1, 0, 7, 0);
+        System.out.println("result (1, 0) to (7, 0) are " + result3);
     }
 }
--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -4,16 +4,15 @@ public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        double s = l * h;
-        return s;
+        return l * h;
     }
 
     public static void main(String[] args) {
-        double s = SqArea.square(4, 1);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + s);
-        double s1 = SqArea.square(12, 2);
-        System.out.println(" p = 12, k = 2, s = 8, real = " + s1);
-        double s2 = SqArea.square(20, 1.5);
-        System.out.println(" p = 20, k = 1.5, s = 24, real = " + s2);
+        double result = SqArea.square(4, 1);
+        System.out.println(" p = 4, k = 1, s = 1, real = " + result);
+        double result1 = SqArea.square(12, 2);
+        System.out.println(" p = 12, k = 2, s = 8, real = " + result1);
+        double result2 = SqArea.square(20, 1.5);
+        System.out.println(" p = 20, k = 1.5, s = 24, real = " + result2);
         }
 }
